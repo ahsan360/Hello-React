76a64f32e01dde1f310c747654b1f33ce09718b1
Ahsan <ahsanhabib462@gmail.com>
2024-02-25 16:26:57 +0600

    bit Changess

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -13,7 +13,7 @@ const SearchParams = () => {
   }, [animal]);
   async function requestPets() {
     const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+      `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
     const json = await res.json();
     setPets(json.pets);
